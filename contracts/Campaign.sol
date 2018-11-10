e8d1e87573005e0aca19e2303d77736d07338cd6
shrys <shreyas770@yahoo.in>
2018-11-10 13:29:49 +0530

    Approve request

--- contracts/Campaign.sol
+++ contracts/Campaign.sol
@@ -6,6 +6,8 @@
             uint value;
             address recipient;
             bool complete;
+            uint approvalCount;
+            mapping(address => bool) apporvals;
         }
         
         Request[] public requests;
@@ -32,8 +34,18 @@
         function createRequest(string description, uint value, address recipient)
         public restricted {
             // require(approvers[msg.sender]);
-            Request memory newRequest = Request(description, value, recipient, false);
+            Request memory newRequest = Request(description, value, recipient, false, 0);
 
             requests.push(newRequest);
+        }
+
+        function approveRequest(uint index) public {
+            Request storage request = requests[index];
+
+            require(approvers[msg.sender]);
+            require(!request.apporvals[msg.sender]);
+            
+            request.apporvals[msg.sender] = true;
+            request.approvalCount++;
         }
     }
