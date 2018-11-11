35ded12c65e404adf24a2a7ca425551e0d20fa5c
shrys <shreyas770@yahoo.in>
2018-11-11 21:48:40 +0530

    Add CampaignFactory contract

--- contracts/Campaign.sol
+++ contracts/Campaign.sol
@@ -1,63 +1,76 @@
-    pragma solidity ^0.4.17;
+pragma solidity ^0.4.17;
 
-    contract Campaign {
-        struct Request {
-            string description;
-            uint value;
-            address recipient;
-            bool complete;
-            uint approvalCount;
-            mapping(address => bool) apporvals;
-        }
-        
-        Request[] public requests;
-        address public manager;
-        uint public minimumContributuion;
-        mapping (address=>bool) public approvers;
-        uint public approversCount;
+contract CampaignFactory {
+    address[] public deployedCampaigns;
 
-        modifier restricted() {
-            require(msg.sender == manager);
-            _;
-        }
-        
-        constructor(uint minimum) public {
-            manager = msg.sender;
-            minimumContributuion = minimum;
-        }
-        
-        function contribute() public payable {
-            require(msg.value > minimumContributuion);
-            
-            approvers[msg.sender] = true;
-            approversCount++;
-        }
-
-        function createRequest(string description, uint value, address recipient)
-        public restricted {
-            // require(approvers[msg.sender]);
-            Request memory newRequest = Request(description, value, recipient, false, 0);
-
-            requests.push(newRequest);
-        }
-
-        function approveRequest(uint index) public {
-            Request storage request = requests[index];
-
-            require(approvers[msg.sender]);
-            require(!request.apporvals[msg.sender]);
-            
-            request.apporvals[msg.sender] = true;
-            request.approvalCount++;
-        }
-
-        function finalizeRequest(uint index) public restricted {
-            Request storage request = requests[index];
-
-            require(request.approvalCount > (approversCount/2));
-            require(!request.complete);
-
-            request.recipient.transfer(request.value);
-            request.complete = true;
-        }
+    function createCampaign(uint minimum) public {
+        address newCampaign = new Campaign(minimum, msg.sender);
+        deployedCampaigns.push(newCampaign);
     }
+
+    function getdeployedCampaigns() public view returns (address[]) {
+        return deployedCampaigns;
+    }
+}
+
+contract Campaign {
+    struct Request {
+        string description;
+        uint value;
+        address recipient;
+        bool complete;
+        uint approvalCount;
+        mapping(address => bool) apporvals;
+    }
+    
+    Request[] public requests;
+    address public manager;
+    uint public minimumContributuion;
+    mapping (address=>bool) public approvers;
+    uint public approversCount;
+
+    modifier restricted() {
+        require(msg.sender == manager);
+        _;
+    }
+    
+    constructor(uint minimum, address creator) public {
+        manager = creator;
+        minimumContributuion = minimum;
+    }
+    
+    function contribute() public payable {
+        require(msg.value > minimumContributuion);
+        
+        approvers[msg.sender] = true;
+        approversCount++;
+    }
+
+    function createRequest(string description, uint value, address recipient)
+    public restricted {
+        // require(approvers[msg.sender]);
+        Request memory newRequest = Request(description, value, recipient, false, 0);
+
+        requests.push(newRequest);
+    }
+
+    function approveRequest(uint index) public {
+        Request storage request = requests[index];
+
+        require(approvers[msg.sender]);
+        require(!request.apporvals[msg.sender]);
+        
+        request.apporvals[msg.sender] = true;
+        request.approvalCount++;
+    }
+
+    function finalizeRequest(uint index) public restricted {
+        Request storage request = requests[index];
+
+        require(request.approvalCount > (approversCount/2));
+        require(!request.complete);
+
+        request.recipient.transfer(request.value);
+        request.complete = true;
+    }
+}
